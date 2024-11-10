82a3ebc7c165de86335cb4cc9117836293596cdc
devkaare <kaarehofferdal@gmail.com>
2024-11-10 21:40:14 +0100

    Simplified code

--- main.go
+++ main.go
@@ -3,166 +3,122 @@ package main
 import (
 	"encoding/csv"
 	"encoding/xml"
-	"log"
+	"fmt"
 	"os"
+	"path/filepath"
 )
 
-type String struct {
+type Field struct {
 	Key   string `xml:"Key"`
 	Value string `xml:"Value"`
 }
 
 type Entry struct {
-	String []String `xml:"String"`
+	Notes, Password, Title, URL, UserName string
 }
 
-type Group struct {
-	Name  string  `xml:"Name"`
-	Entry []Entry `xml:"Entry"`
-}
+func Decode(d *xml.Decoder) []Entry {
+	var (
+		result []Entry
+		new    Entry
+		field  Field
+	)
 
-type Groups struct {
-	Group []Group `xml:"Group"`
-}
+	// Parse xml data
+	for t, _ := d.Token(); t != nil; t, _ = d.Token() {
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "String" {
+				d.DecodeElement(&field, &se)
 
-type Root struct {
-	Groups []Groups `xml:"Group"`
-}
-
-type KeePass struct {
-	Root Root `xml:"Root"`
-}
-
-func (r *KeePass) Log() {
-	// Parse over all groups inside root.
-	for _, gs := range r.Root.Groups {
-		// Parse over all the groups inside the main group.
-		for _, gsi := range gs.Group {
-			// Parse over all entries.
-			for _, en := range gsi.Entry {
-				// Parse over all the strings inside the entry.
-				for _, ens := range en.String {
-					// Log all the fields inside.
-					log.Println(ens.Key, ens.Value)
+				// Add x value to new
+				switch field.Key {
+				case "Notes":
+					new.Notes = field.Value
+				case "Password":
+					new.Password = field.Value
+				case "Title":
+					new.Title = field.Value
+				case "URL":
+					new.URL = field.Value
+				case "UserName":
+					new.UserName = field.Value
+					// UserName is the last field for an entry,
+					// so new is appended to result when UserName is found
+					result = append(result, new)
 				}
 			}
 		}
 	}
+
+	// Return all result found
+	return result
 }
 
-func (r *KeePass) Format() [][]string {
-	// Find the length the csvSlice should be.
-	var entryCount int
-	for _, gs := range r.Root.Groups {
-		for _, gsi := range gs.Group {
-			for range gsi.Entry {
-				// Increment csvSlice length counter by 1.
-				entryCount++
-			}
-		}
-	}
+func Format(entries []Entry) [][]string {
+	csvSlice := make([][]string, 1000) // Make a slice to store the XML data.
 
-	// Add 1 more to entry count to account for the csvSlice format fields.
-	entryCount++
+	var i int
 
-	// Make a slice to store the XML data.
-	csvSlice := make([][]string, entryCount)
-
-	// KeePassXCs requierd fields (for CSV format):
-	// Group,Title,Username,Password,URL,Notes,TOTP,Icon,Last Modified,Created
-	csvSlice[0] = []string{
+	csvSlice[i] = []string{ // KeePassXCs requierd fields (for CSV format):
 		"Group",
 		"Title",
 		"Username",
 		"Password",
 		"URL",
 		"Notes",
-		"TOTP",
-		"Icon",
-		"Last Modified",
-		"Created",
 	}
+	i++
 
-	// Create variable to keep track of how many entries have been added.
-	var addedCount int
-
-	// Update count since I added the csvSlice format fields (line 73).
-	addedCount++
-
-	// Parse over all groups inside root.
-	for _, gs := range r.Root.Groups {
-		// Parse over all the groups inside the main group.
-		for _, gsi := range gs.Group {
-			// Parse over all entries.
-			for _, en := range gsi.Entry {
-				entrySlice := make([]string, 10)
-				// Add group name to entry slice.
-				entrySlice[0] = gsi.Name
-
-				// Parse over all the strings inside the entry.
-				for _, ens := range en.String {
-					// Add value depending on keys value.
-					switch ens.Key {
-					case "Title":
-						entrySlice[1] = ens.Value
-					case "UserName":
-						entrySlice[2] = ens.Value
-					case "Password":
-						entrySlice[3] = ens.Value
-					case "URL":
-						entrySlice[4] = ens.Value
-					case "Notes":
-						entrySlice[5] = ens.Value
-					}
-				}
-				// Add entry slice to csvSlice (the slice that will be returned as result).
-				csvSlice[addedCount] = entrySlice
-
-				// Update added entry count.
-				addedCount++
-			}
+	for _, entry := range entries { // Add new entries
+		csvSlice[i] = []string{
+			"Root", // Root is the group entries will be stored in
+			entry.Title,
+			entry.UserName,
+			entry.Password,
+			entry.URL,
+			entry.Notes,
 		}
+		i++
 	}
 
-	// Return csvSlice as CSV formatted result.
 	return csvSlice
 }
 
 func main() {
-	// TODO: Add command-line input and multi-structure support
-	fileName := "example"
 	fileDir := "tmp/"
-	// Open KeePassXC XML file.
-	xmlData, err := os.ReadFile(fileDir + fileName + ".xml") // Read file.
+	var fileName string
+	fmt.Println("What's the name of the XML file you want to convert? e.g. passwords")
+	fmt.Scanln(&fileName)
+
+	fmt.Println("Opening file...")
+	xmlFile, err := os.Open(filepath.Join(fileDir, (fileName + ".xml")))
 	if err != nil {
-		log.Fatal(err)
+		panic("Failed to open XML file")
 	}
 
-	// Create a struct with the format for the XML data.
-	var newData KeePass
+	// Decode
+	fmt.Println("Decoding entries...")
+	d := xml.NewDecoder(xmlFile)
+	entries := Decode(d)
 
-	// Unmarshal the XML data into the struct.
-	if err = xml.Unmarshal(xmlData, &newData); err != nil {
-		log.Fatal(err)
-	}
-
-	// Log the XML data in a readable format.
-	//newData.Log()
-
-	// Create a CSV file.
-	csvFile, err := os.Create(fileDir + fileName + ".csv")
+	// Create a CSV file
+	fmt.Println("Writing output to file...")
+	csvFile, err := os.Create(filepath.Join(fileDir, (fileName + ".csv")))
 	if err != nil {
-		log.Fatal(err)
+		panic("Failed to open CSV file")
 	}
 
-	// Format the XML data into a CSV format.
-	csvData := newData.Format()
+	// Format the XML data into a CSV format
+	csvData := Format(entries)
 
-	// Write formatted data to the csv file.
+	// Write formatted data to the csv file
 	writer := csv.NewWriter(csvFile)
 	writer.WriteAll(csvData)
 
 	if err := writer.Error(); err != nil {
-		log.Fatal(err)
+		panic("Failed to write to CSV file")
 	}
+
+	fmt.Println("Successfully converted XML file to CSV file!")
 }
